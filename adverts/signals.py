2b848e5ec556be3f5b38a459ae63a8d066140d3f
2nky <121765538+2nky@users.noreply.github.com>
2023-08-14 19:21:16 +0200

    Отправим письмо автору при создании отклика на его объявление

--- adverts/signals.py
+++ adverts/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 
-from .models import Advert
+from .models import Advert, Reply
 
 
 @receiver(post_save, sender=Advert)
@@ -19,5 +19,22 @@ def notify_new_advert(sender, instance, **kwargs):
         ),
         "noreply@adboard.fake",
         [instance.author.email],
+        fail_silently=False,
+    )
+
+
+@receiver(post_save, sender=Reply)
+def notify_new_reply(sender, instance, **kwargs):
+    send_mail(
+        "Кто-то откликнулся на ваше объявление",
+        render_to_string(
+            template_name="replies/new_reply.txt",
+            context={
+                "reply": instance,
+                "advert": instance.advert,
+            },
+        ),
+        "noreply@adboard.fake",
+        [instance.advert.author.email],
         fail_silently=False,
     )
